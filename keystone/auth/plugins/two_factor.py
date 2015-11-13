c727449ad08ab71492218c4da6552db5d60e8a2e
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-11-13 17:21:20 +0100

    Two factor auth plugin working
    Somes fixes in auth tests were also made

--- keystone/auth/plugins/two_factor.py
+++ keystone/auth/plugins/two_factor.py
@@ -12,20 +12,23 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from keystone.openstack.common import log
+import sys
 
-from keystone.common import dependency
+import six
+
 from keystone.auth.plugins.password import UserAuthInfo,Password
+from keystone.common import dependency
 from keystone import exception
 from keystone.i18n import _
+from keystone.openstack.common import log
 
 
 LOG = log.getLogger(__name__)
 
 METHOD_NAME = 'two_factor'
-
-@dependency.requires('identity_api')
-class UserTwoFactorAuthInfo(UserAuthInfo):
+      
+@dependency.requires('assignment_api', 'identity_api')
+class UserTwoFactorAuthInfo(object):
     @staticmethod
     def create(auth_payload):
         user_auth_info = UserTwoFactorAuthInfo()
@@ -38,11 +41,81 @@ class UserTwoFactorAuthInfo(UserAuthInfo):
         self.user_ref = None
         self.time_based_code = None
 
+    def _assert_domain_is_enabled(self, domain_ref):
+        try:
+            self.assignment_api.assert_domain_enabled(
+                domain_id=domain_ref['id'],
+                domain=domain_ref)
+        except AssertionError as e:
+            LOG.warning(e)
+            six.reraise(exception.Unauthorized, exception.Unauthorized(e),
+                        sys.exc_info()[2])
+
+    def _assert_user_is_enabled(self, user_ref):
+        try:
+            self.identity_api.assert_user_enabled(
+                user_id=user_ref['id'],
+                user=user_ref)
+        except AssertionError as e:
+            LOG.warning(e)
+            six.reraise(exception.Unauthorized, exception.Unauthorized(e),
+                        sys.exc_info()[2])
+
+    def _lookup_domain(self, domain_info):
+        domain_id = domain_info.get('id')
+        domain_name = domain_info.get('name')
+        domain_ref = None
+        if not domain_id and not domain_name:
+            raise exception.ValidationError(attribute='id or name',
+                                            target='domain')
+        try:
+            if domain_name:
+                domain_ref = self.assignment_api.get_domain_by_name(
+                    domain_name)
+            else:
+                domain_ref = self.assignment_api.get_domain(domain_id)
+        except exception.DomainNotFound as e:
+            LOG.exception(e)
+            raise exception.Unauthorized(e)
+        self._assert_domain_is_enabled(domain_ref)
+        return domain_ref
+
     def _validate_and_normalize_auth_data(self, auth_payload):
+        if 'user' not in auth_payload:
+            raise exception.ValidationError(attribute='user',
+                                            target=METHOD_NAME)
+        user_info = auth_payload['user']
+        user_id = user_info.get('id')
+        user_name = user_info.get('name')
+        user_ref = None
+        if not user_id and not user_name:
+            raise exception.ValidationError(attribute='id or name',
+                                            target='user')
+        self.password = user_info.get('password')
+        try:
+            if user_name:
+                if 'domain' not in user_info:
+                    raise exception.ValidationError(attribute='domain',
+                                                    target='user')
+                domain_ref = self._lookup_domain(user_info['domain'])
+                user_ref = self.identity_api.get_user_by_name(
+                    user_name, domain_ref['id'])
+            else:
+                user_ref = self.identity_api.get_user(user_id)
+                domain_ref = self.assignment_api.get_domain(
+                    user_ref['domain_id'])
+                self._assert_domain_is_enabled(domain_ref)
+        except exception.UserNotFound as e:
+            LOG.exception(e)
+            raise exception.Unauthorized(e)
+        self._assert_user_is_enabled(user_ref)
+        self.user_ref = user_ref
+        self.user_id = user_ref['id']
+        self.domain_id = domain_ref['id']
         if 'time_based_code' not in auth_payload:
             raise exception.ValidationError(attribute='user', target=METHOD_NAME)
         self.time_based_code = auth_payload['time_based_code']
-        #super(UserTwoFactorAuthInfo, self)._validate_and_normalize_auth_data(auth_payload=auth_payload)
+
 
 @dependency.requires('two_factor_auth_api')
 class TwoFactor(Password):
@@ -52,25 +125,18 @@ class TwoFactor(Password):
     def authenticate(self, context, auth_payload, auth_context):
         """Two factor authentication"""
 
-        LOG.info("Authenticating with twofactor")
-
         if not self.two_factor_auth_api:
             raise exception.Unauthorized(_('%s not supported') % self.method)
 
-        user_id = auth_payload['user']['id']
-
-        LOG.info("User is "+str(user_id))
+        user_info = UserAuthInfo.create(auth_payload)
+        user_id = user_info.user_id
 
         try:
             self.two_factor_auth_api.is_two_factor_enabled(user_id)
         except exception.NotFound:
-            LOG.info("twofactor is not enabled.")
             return super(TwoFactor, self).authenticate(context, auth_payload, auth_context)
 
-        LOG.info("User has twofactor enabled")
         user_info = UserTwoFactorAuthInfo.create(auth_payload)
-
-        LOG.info("Code to try is "+str(user_info.time_based_code))
 
         if not self.two_factor_auth_api.verify_code(user_id, user_info.time_based_code):
             raise exception.Unauthorized(_('Invalid time based code'))
